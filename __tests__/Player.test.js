84c6a4767abc4c86c3e785dbb9b58e5d149abb24
jesse sheehan <jessetsheehan@gmail.com>
2022-06-15 15:19:57 -0400

    added some player methods and corresponding tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -37,3 +37,31 @@ test("Gets inventory from player or returns false", () => {
 
     expect(player.getInventory()).toEqual(false);
 })
+
+
+test("gets player's health value", () => {
+    const player = new Player("Dave");
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("check if player is alive or not", () => {
+    const player = new Player("Dave");
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtract from player's health", () => {
+    const player = new Player("Dave");
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+})
